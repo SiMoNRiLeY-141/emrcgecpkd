5726c7a6a07a3d60701e906c9ae9968c9f241c2a
Adhil Jahan <adhiljahan2@gmail.com>
2024-05-29 18:20:42 +0530

    Google console

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React, { Suspense, lazy } from 'react';
-import Head from 'next/head';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
+import React, { Suspense, lazy } from "react";
+import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
-const Header = lazy(() => import('../components/Header'));
+const Header = lazy(() => import("../components/Header"));
 const SocialMediaOverlay = lazy(
-  () => import('../components/SocialMediaOverlay')
+  () => import("../components/SocialMediaOverlay"),
 );
-const Committee = lazy(() => import('../components/Committee'));
-const ContactSection = lazy(() => import('../components/ContactSection'));
-const News = lazy(() => import('../components/News'));
-const Newsletter = lazy(() => import('../components/Newsletter'));
+const Committee = lazy(() => import("../components/Committee"));
+const ContactSection = lazy(() => import("../components/ContactSection"));
+const News = lazy(() => import("../components/News"));
+const Newsletter = lazy(() => import("../components/Newsletter"));
 
 const HomePage = () => {
   return (
@@ -21,6 +21,10 @@ const HomePage = () => {
         <link
           rel="icon"
           href="https://tptqglihfsppnrrtukvw.supabase.co/storage/v1/object/sign/assets/logos/favicon.ico?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhc3NldHMvbG9vcy9mYXZpY29uLmljbyIsImlhdCI6MTcxNjM4OTk1NSwiZXhwIjoyMDMxNzQ5OTU1fQ.pS7XAYfr9OuB7EwJiMEemuRjUfstb7EPp0MUetEFixg&t=2024-05-22T14%3A59%3A16.270Z"
+        />
+        <meta
+          name="google-site-verification"
+          content="RsmQDkTKhMkEqmP3ipd5IGfsQoeIZd3glUMDWGcEhUI"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
