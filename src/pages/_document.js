d63316ba88c29086d69ad630f4d79926c44af8a6
Adhil Jahan <adhiljahan2@gmail.com>
2024-06-05 11:17:59 +0530

    BG

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,24 +6,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Microsoft Clarity */}
-          <script
-            defer
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "mmplpjhvb0");
-              `,
-            }}
-          />
           {/* Google Analytics */}
           <script
             async
-            defer
             src="https://www.googletagmanager.com/gtag/js?id=G-GFJQXE1T6K"
           ></script>
           <script
