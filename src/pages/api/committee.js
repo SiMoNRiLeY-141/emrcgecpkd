40e2dc8a1b3c671152f4eaaaf2ee718a750ec5de
ADHIL JAHAN <adhiljahan2@gmail.com>
2026-01-25 13:43:53 +0530

    Order

--- src/pages/api/committee.js
+++ src/pages/api/committee.js
@@ -2,12 +2,15 @@ import supabase from './supabase';
 
 export default async function handler(req, res) {
   try {
-    const { data, error } = await supabase.from('committee').select('*');
-      if (error) {
+    const { data, error } = await supabase
+      .from('committee')
+      .select('*')
+      .order('id', { ascending: true });
+    if (error) {
       throw error;
-      }
-      res.status(200).json(data);
-    } catch (error) {
+    }
+    res.status(200).json(data);
+  } catch (error) {
     res.status(500).json({ error: error.message });
-    }
-    }
+  }
+}
